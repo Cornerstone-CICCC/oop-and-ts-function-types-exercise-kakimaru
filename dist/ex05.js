d6276a980a2387c511115e738fe486d4951da415
kakimaru <memimei0306@gmail.com>
2024-10-05 08:23:00 +0900

    I tried exercises

---
+++ dist/ex05.js
@@ -0,0 +1,11 @@
+"use strict";
+// Create a function called sumAllNumbers.
+// This function should accept any number of numbers using a rest parameter.
+// It should return the sum of all the numbers passed to it.
+function sumAllNumbers(...numbers) {
+    const sum = numbers.reduce((acc, cur) => {
+        return acc + cur;
+    }, 0);
+    return sum;
+}
+console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
